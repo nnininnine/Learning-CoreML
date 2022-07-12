812501c4411cff66215e168a04bb532920570cbf
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 09:08:42 +0700

    Initial Commit

---
+++ Learning-CoreML/Learning_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_CoreMLApp.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 12/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Learning_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

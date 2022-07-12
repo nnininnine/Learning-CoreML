97c3594fdb34f61e878023d7c548afd18aee7457
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 09:55:08 +0700

    feat: implement PhotoButtonGroup

---
+++ Learning-CoreML/View/PhotoButtonGroup.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoButtonGroup.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 12/7/2565 BE.
+//
+
+import SwiftUI
+
+struct PhotoButtonGroup: View {
+  // MARK: Properties
+
+  var onTapTakePhoto: () -> Void
+  var onTapGallery: () -> Void
+
+  // MARK: Body
+
+  var body: some View {
+    HStack(spacing: 32) {
+      Button {
+        onTapTakePhoto()
+      } label: {
+        Text("Take Photo")
+          .foregroundColor(Color(uiColor: .label))
+      }
+      .padding()
+
+      Button {
+        onTapGallery()
+      } label: {
+        Text("Gallery")
+          .foregroundColor(Color(uiColor: .label))
+      }
+      .padding()
+    }
+    .background(.ultraThinMaterial)
+    .cornerRadius(12)
+  }
+}
+
+struct PhotoButtonGroup_Previews: PreviewProvider {
+  static var previews: some View {
+    PhotoButtonGroup(onTapTakePhoto: {}, onTapGallery: {})
+      .preferredColorScheme(.dark)
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}

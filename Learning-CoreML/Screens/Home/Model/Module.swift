1d55a33162b7266739f3e842436e7626c264033b
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-14 09:32:31 +0700

    feat(home): define Module model

---
+++ Learning-CoreML/Screens/Home/Model/Module.swift
@@ -0,0 +1,15 @@
+//
+//  Module.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 14/7/2565 BE.
+//
+
+import SwiftUI
+
+typealias Modules = [Module]
+
+struct Module {
+  var name: String
+  var view: AnyView
+}

db85a24acd1cd6ae73c8c731fb9b78e1e96f2bd1
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-14 09:13:11 +0700

    feat: add HomeView, move some file

---
+++ Learning-CoreML/Screens/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 14/7/2565 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}

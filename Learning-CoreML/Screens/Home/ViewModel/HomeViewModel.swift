297e295b74442f6129a9386dbd3b3c1db8d7b829
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 09:56:20 +0700

    feat(home): add SimpleTokenizationView in HomeViewModel

--- Learning-CoreML/Screens/Home/ViewModel/HomeViewModel.swift
+++ Learning-CoreML/Screens/Home/ViewModel/HomeViewModel.swift
@@ -12,7 +12,8 @@ class HomeViewModel: ObservableObject {
 
   let modules: Modules = [
     Module(name: "Image Classification", view: AnyView(ImageClassificationView())),
-    Module(name: "Sign Language Classification", view: AnyView(SignLangClassificationView()))
+    Module(name: "Sign Language Classification", view: AnyView(SignLangClassificationView())),
+    Module(name: "Simple Tokenization", view: AnyView(SimpleTokenizationView()))
   ]
 
   // MARK: Methods
--- Learning-CoreML/Screens/SimpleTokenization/View/SimpleTokenizationView.swift
+++ Learning-CoreML/Screens/SimpleTokenization/View/SimpleTokenizationView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct SimpleTokenization: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct SimpleTokenizationView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 struct SimpleTokenization_Previews: PreviewProvider {
-    static var previews: some View {
-        SimpleTokenization()
-    }
+  static var previews: some View {
+    SimpleTokenizationView()
+  }
 }

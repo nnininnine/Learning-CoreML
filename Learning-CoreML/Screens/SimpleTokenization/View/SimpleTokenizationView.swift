4d71303f61b98a26c97f4314b59f56f21c8fff93
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 09:53:25 +0700

    feat: create SimpleTokenizationView

---
+++ Learning-CoreML/Screens/SimpleTokenization/View/SimpleTokenizationView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleTokenization.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+struct SimpleTokenization: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleTokenization_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleTokenization()
+    }
+}

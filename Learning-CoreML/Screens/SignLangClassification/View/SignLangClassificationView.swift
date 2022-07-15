a28d418ce7361b30f6ce634961144d3029f6b0bc
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 09:43:58 +0700

    feat: create SignLangClassificationView

---
+++ Learning-CoreML/Screens/SignLangClassification/View/SignLangClassificationView.swift
@@ -0,0 +1,21 @@
+//
+//  SignLangClassificationView.swift
+//  Learning-CoreML
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+struct SignLangClassificationView: View {
+  var body: some View {
+    Text("Sign Language Classification")
+      .navigationTitle("Sign Language")
+  }
+}
+
+struct SignLangClassificationView_Previews: PreviewProvider {
+  static var previews: some View {
+    SignLangClassificationView()
+  }
+}
